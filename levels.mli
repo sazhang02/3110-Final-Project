3f9851a5244c25860d736a0d15406dc05fb0e461
Brooke Wu <bw377@cornell.edu>
2021-03-24 23:53:16 -0500

    update levels.mli & empty_level.json

--- levels.mli
+++ levels.mli
@@ -6,27 +6,28 @@ type t
 (** The type representing a position in a level. *)
 type pos = int * int
 
-(** The type of level identifier. *)
-type level_id = string
+(** The type of level identifier. Each level's [level_id] is unique.*)
+type level_id = int
 
 (** Raised when an unknown level is met. *)
 exception UnknownLevel of level_id
 
+(** [from_json j] is the set of levels that [j] represents. Requires:
+    [j] is a valid json representation.*)
 val from_json : Yojson.Basic.t -> t
 
-(** [entrance_pipe] is the position [pos] of the entrance pipe of the level [t]. 
-*)
-val entrance_pipe : t-> pos
+(** [entrance_pipe levels lev] is the position [pos] of the entrance
+    pipe of the level [lev] in levels [levels]. *)
+val entrance_pipe : t -> level_id -> pos
 
-(** [exit_pipe] is the position [pos] of the exit pipe of the level [t]. *)
-val exit_pipe : t -> pos
+(** [exit_pipe levels lev] is the position [pos] of the exit pipe of the
+    level [lev] in levels [levels]. *)
+val exit_pipe : t -> level_id -> pos
 
-(** [next_level] is the [level_id] of the level to which the level [t] exits.
-  Raises [UnkownLevel] if [t] is the last level.
-*)
-val next_level : t -> level_id
+(** [next_level levels id] is the [level_id] of the level to which the
+    level [lev] exits. Raises [UnkownLevel] if [lev] is the last level. *)
+val next_level : t -> level_id -> level_id
 
-(** [prev_level] is the [level_id] of the level from which the level [t] enters.
-  Raises [UnknownLevel] if [t] is the starting level.
-*)
+(** [prev_level] is the [level_id] of the level from which the level
+    [lev] enters. Raises [UnknownLevel] if [lev] is the starting level. *)
 val prev_level : t -> level_id
