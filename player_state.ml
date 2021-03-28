a368b67cd4d0e09be03f189f9caae05527f24e03
el572@cornell.edu <el572@cornell.edu>
2021-03-28 05:10:11 -0400

    Added preliminary position and state updates in Player_state also added getter functions for Gui.coords in Gui

--- player_state.ml
+++ player_state.ml
@@ -1,22 +1,63 @@
 open Yojson.Basic.Util
 open Levels
+open Gui
 
 type level_id = Levels.level_id
 
-type position = int * int
+type position = Gui.coords
+
+type coins = int
 
 exception UnknownPosition of position
 
 type p = {
   current_pos : position;
   current_level : level_id;
+  coins : coins;
 }
 
-let init_state levels = failwith "Unimplemented"
+let init_state lev lev_id = entrance_pipe lev lev_id
 
 let get_current_level (ps : p) = ps.current_level
 
 let get_current_pos (ps : p) = ps.current_pos
+
+let update_pos (move : char) (p : p) =
+  match move with
+  | 'w' -> { x = get_x p.current_pos; y = get_y p.current_pos + 1 }
+  | 's' -> { x = p.current_pos.x; y = get_y p.current_pos - 1 }
+  | 'a' -> { x = get_x p.current_pos - 1; y = get_y p.current_pos }
+  | 'd' -> { x = get_x p.current_pos + 1; y = get_y p.current_pos }
+  | _ -> failwith "Not a valid move."
+
+(* Take char as input from gui.ml, return new state *)
+let update p =
+  match Graphics.read_key () with
+  | 'w' ->
+      {
+        current_pos = update_pos 'w' p;
+        current_level = p.current_level;
+        coins = p.coins;
+      }
+  | 's' ->
+      {
+        current_pos = update_pos 'w' p;
+        current_level = p.current_level;
+        coins = p.coins;
+      }
+  | 'a' ->
+      {
+        current_pos = update_pos 'w' p;
+        current_level = p.current_level;
+        coins = p.coins;
+      }
+  | 'd' ->
+      {
+        current_pos = update_pos 'w' p;
+        current_level = p.current_level;
+        coins = p.coins;
+      }
+  | _ -> failwith "Not a valid move."
 
 type result =
   | Legal of p
--- player_state.mli
+++ player_state.mli
@@ -5,7 +5,7 @@ type p
 type level_id = int
 
 (* The type of player's position: an (x,y) coordinate in the GUI grid *)
-type position = int * int
+type position = Gui.coords
 
 (** Raised when an unknown position is met. *)
 exception UnknownPosition of position
@@ -13,7 +13,7 @@ exception UnknownPosition of position
 (** [init_state a] is the initial state of the game when playing in each
     level. In that state the character is currently located in the
     starting position, and they have visited only that level. *)
-val init_state : Levels.t -> p
+val init_state : Levels.t -> Levels.level_id -> Levels.tile
 
 (** [get_current_level p] is the identifier of the level in which the
     adventurer currently is located in state [ps]. *)
@@ -22,6 +22,10 @@ val get_current_level : p -> Levels.level_id
 (** [get current_pos p] is the position in which the character currently
     is located in state [p]. *)
 val get_current_pos : p -> position
+
+val update_pos : char -> p -> position
+
+val update : p -> p
 
 (** The type representing the result of an attempted movement. *)
 type result =
