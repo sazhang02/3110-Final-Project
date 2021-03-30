fcc9160e6128b31b32365d34ba34e20e430e6b1c
Samantha Zhang <sjz46@cornell.edu>
2021-03-30 15:54:09 -0400

    Created authors.mli and authors.mli

---
+++ authors.mli
@@ -0,0 +1,14 @@
+(************************************************************ 
+   You may not redistribute this project, distribute any derivatives,
+   or use it for commercial purposes.
+ ************************************************************)
+
+(** CS 3110 Final Project - Stuck In The Desert
+
+    @author Samantha Zhang (sjz46)
+    @author Brooke Wu (bw377
+    @author Willy Jiang (wjj26)
+    @author Emily Liu (el572) *)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
