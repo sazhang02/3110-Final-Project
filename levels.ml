3963bbf29929d4d9f417ae407ae007a7106b83c6
Brooke Wu <bw377@cornell.edu>
2021-03-30 15:53:02 -0500

    implement levels.next_level & .prev_level & tests

--- levels.ml
+++ levels.ml
@@ -17,23 +17,18 @@ type level = {
   level_id : level_id;
   entrance_pos : pos;
   exit_pos : pos;
-  exit_id : level_id; (* objects : tile list; *)
-}
-
-type t = {
-  levels : level list;
+  exit_id : level_id;
+  (* objects : tile list; *)
   board : board;
 }
 
+type t = { levels : level list }
+
 exception UnknownLevel of level_id
 
+exception InvalidTile of pos
+
 open Yojson.Basic.Util
-
-(* let list_to_tuple = function [] -> (0, 0) | h :: t -> (h, List.hd t) *)
-
-(* let to_tuple str = String.length str - 2 |> String.sub str 1 |>
-   String.split_on_char ',' |> List.map String.trim |> List.map
-   int_of_string |> list_to_tuple *)
 
 let get_pos tile = tile.pos
 
@@ -42,20 +37,21 @@ let get_tile_type tile = tile.tile_type
 let pos_of_json_tile j =
   { x = j |> member "x" |> to_int; y = j |> member "y" |> to_int }
 
+(**TODO: change board type*)
 let level_of_json j =
   {
     level_id = j |> member "id" |> to_int;
     entrance_pos = j |> member "entrance" |> pos_of_json_tile;
     exit_pos = j |> member "exit" |> pos_of_json_tile;
     exit_id = j |> member "exit_id" |> to_int;
-  }
-
-let from_json j =
-  {
-    levels = j |> member "levels" |> to_list |> List.map level_of_json;
     board = ();
   }
 
+let from_json j =
+  { levels = j |> member "levels" |> to_list |> List.map level_of_json }
+
+(** [map_level id t f] is [f] applied to the level that [id] corresponds
+    to in [t]*)
 let rec map_level id level_list f =
   match level_list with
   | [] -> raise (UnknownLevel id)
@@ -73,14 +69,22 @@ let entrance_pipe (levels : t) (id : level_id) : tile =
 
 let exit_pos level = level.exit_pos
 
-(**TODO: change tile_type to pipe. Decide how to handle final level when
-   there is no exit. *)
+(**TODO: change tile_type to pipe. *)
 let exit_pipe (levels : t) (id : level_id) : tile =
-  to_tile levels id exit_pos ()
+  let exit_tile = to_tile levels id exit_pos () in
+  if exit_tile.pos.x = -1 || exit_tile.pos.y = -1 then
+    raise (InvalidTile exit_tile.pos)
+  else exit_tile
 
-(** empty str means exit does not exist????*)
+let exit_id level = level.exit_id
+
+let check_level_validity id =
+  if id = -1 then raise (UnknownLevel id) else id
+
 let next_level (levels : t) (id : level_id) : level_id =
-  failwith "Unimplemented"
+  map_level id levels.levels exit_id |> check_level_validity
+
+let level_id level = level.level_id
 
 let prev_level (levels : t) (id : level_id) : level_id =
-  failwith "Unimplemented"
+  map_level id levels.levels level_id - 1 |> check_level_validity
