20dd2dbc43619daac5fa630f7dbe666100d42d28
Brooke Wu <bw377@cornell.edu>
2021-05-19 22:17:40 -0500

    homescreen with no images

--- homescreen.ml
+++ homescreen.ml
@@ -1,45 +1,56 @@
 open Graphics
+open Gui
 
 (* let homescreen () : string * Levels.level_id = failwith "" *)
-type button_type =
-  | Start
-  | Id of Levels.level_id
-  | File of string
 
 type button = {
   x : int;
   y : int;
   h : int;
   w : int;
-  selected : bool;
-  typ : button_type;
 }
 
 let init = ("playtest_levels.json", 0)
 
-(* true is easy, false is hard *)
-type game_state = { m : bool }
+let is_easy_game = ref true
 
-let current_game_state = ref { m = true }
+let center_pos =
+  let x_y = get_window_size Large in
+  let x = fst x_y in
+  let y = snd x_y in
+  (x / 2, y / 2)
 
-let level_0 =
-  { x = 400; y = 400; h = 70; w = 70; selected = false; typ = Id 0 }
-
-let hard_mode =
-  ref
-    {
-      x = 600;
-      y = 600;
-      h = 70;
-      w = 70;
-      selected = false;
-      typ = File "game_levels.json";
-    }
+let start_img_w_h = (300, 100)
 
 let start =
-  { x = 200; y = 200; h = 70; w = 100; selected = false; typ = Start }
+  {
+    x = fst center_pos - (fst start_img_w_h / 2);
+    y = snd center_pos - (snd start_img_w_h / 2);
+    h = snd start_img_w_h;
+    w = fst start_img_w_h;
+  }
 
-let file_name = "playtest_levels.json"
+let offset = 10
+
+let easy_mode =
+  {
+    x = start.x;
+    y = start.y - 100;
+    h = start.h / 2;
+    w = (start.w / 2) - offset;
+  }
+
+let hard_mode =
+  {
+    x = start.x + easy_mode.w + (2 * offset);
+    y = start.y - 100;
+    h = start.h / 2;
+    w = (start.w / 2) - offset;
+  }
+
+let easy_game = "playtest_levels.json"
+
+let hard_game = "game_levels.json"
 
 let is_in_range pos button =
   let x = fst pos in
@@ -51,9 +62,7 @@ let is_in_range pos button =
 
 (*utop code #require "graphics";; open Graphics;; open_graph "
   1100x800";; *)
-let click_button button e =
-  (* if is_in_range (e.mouse_x, e.mouse_y) button && e.button then {
-     button with selected = not button.selected } else button *)
+let button_is_clicked button e =
   is_in_range (e.mouse_x, e.mouse_y) button && e.button
 
 let draw_button b = draw_rect b.x b.y b.w b.h
@@ -65,66 +74,32 @@ let rec draw_all_buttons b_lst =
       draw_rect h.x h.y h.w h.h;
       draw_all_buttons t
 
-(*looping button, use for toggles i think*)
-let rec toggle () button =
+let rec choose_mode () =
   let e = wait_next_event [ Button_down; Button_up; Key_pressed ] in
-  let bu =
-    if click_button button e then
-      { button with selected = not button.selected }
-    else button
-  in
-  print_endline (string_of_bool bu.selected);
-  if bu.selected then (
-    (* match bu.typ with | File f -> (f, snd init) | Id i -> (fst init,
-       i) | Start -> init *)
-    clear_graph ();
-    draw_button bu;
-    draw_circle 100 100 100 )
-  else (
-    (* init *)
-    clear_graph ();
-    draw_button bu;
-    draw_rect 100 100 100 100 );
-  if (not bu.selected) && not (bu.typ = Start) then toggle () bu else ()
+  if button_is_clicked start e |> not then
+    if button_is_clicked hard_mode e then (
+      is_easy_game := false;
+      print_endline (string_of_bool !is_easy_game);
+      choose_mode ())
+    else if button_is_clicked easy_mode e then (
+      is_easy_game := true;
+      print_endline (string_of_bool !is_easy_game);
+      choose_mode ())
+    else choose_mode ()
+  else ()
 
-let rec toggle2 () button st =
-  let e = wait_next_event [ Button_down; Button_up ] in
-  let bu =
-    if click_button button e then
-      { button with selected = not button.selected }
-    else button
-  in
-  let st' =
-    match bu.typ with
-    | File f -> toggle2 () bu (f, snd st)
-    | Id i -> toggle2 () bu (fst st, i)
-    | Start -> st
-  in
-  print_endline (fst st' ^ string_of_int (snd st'));
-  st'
+let get_mode is_easy =
+  (* print_endline ("bonk" ^ string_of_bool !is_easy_game); *)
+  if is_easy then easy_game else hard_game
 
-(* if (not bu.selected) && not (bu.typ = Start) then toggle2 () bu else
-   () *)
-
-(* let st' = if bu.selected then match bu.typ with | File f -> toggle2
-   () bu (f, snd st) | Id i -> toggle2 () bu (fst st, i) | Start -> st
-   else st in print_endline (fst st' ^ string_of_int (snd st')); st' *)
-
-let rec proceed () =
-  let e = wait_next_event [ Button_down; Button_up ] in
-  if click_button start e then (file_name, 0) else proceed ()
+let proceed () =
+  print_endline ("is easy? " ^ string_of_bool !is_easy_game);
+  print_endline ("mode " ^ get_mode !is_easy_game);
+  (get_mode !is_easy_game, 0)
 
 let homescreen () =
-  (* open_graph ""; *)
   draw_button start;
 
-  (* draw_all_buttons [ start; !hard_mode ]; *)
+  draw_all_buttons [ start; hard_mode; easy_mode ];
+  choose_mode ();
   proceed ()
-
-(* toggle () hard_mode; *)
-(* toggle2 () !hard_mode init *)
-
-(* clear_graph (); *)
-
-(* draw_button b; loop () b; *)
-(* close_graph () *)
